16ed11bee897e7381d0f711c4f342fb1d42bb2cb
AugustExner <exneraugust@gmail.com>
2025-04-04 10:33:56 +0200

    removed comments and changed naming of array variables

--- parkingMap.js
+++ parkingMap.js
@@ -1,71 +1,11 @@
 const map = L.map("map").setView([56.17254, 10.189626], 18);
 let parkingSpots = [];
 let parkingCircles = {}; // Store circles by SpotID
-let walkingCoordsArray = [
-  //START SLUT Helsingforsgade
-  // { lat: 56.171378, lng: 10.190248 },
-  // { lat: 56.171788, lng: 10.187843 },
-  // //BIL 1 Helsingforsgade
-  // { lat: 56.171511, lng: 10.189496 },
-  // { lat: 56.171627, lng: 10.189445 },
-  // //BIL 2 Helsingforsgade
-  // { lat: 56.171709, lng: 10.188522 },
-  // { lat: 56.171856, lng: 10.188469 },
-  // //START SLUT Findlandsgade
-  // { lat: 56.17321, lng: 10.191221 },
-  // { lat: 56.171499, lng: 10.190689 },
-  // //BIL 1 Findlandsgade
-  // { lat: 56.172735, lng: 10.19085 },
-  // { lat: 56.172635, lng: 10.191075 },
-  // //BIL 2 Findlandsgade
-  // { lat: 56.172504, lng: 10.190785 },
-  // { lat: 56.172472, lng: 10.190895 },
-  //DEBUG - START/SLUT FINDLANDSGADE
-  // { lat: 56.171588723, lng: 10.190328373 },
-  // { lat: 56.171579336, lng: 10.190250421 },
-  // //DEBUG Bil 1 FINDLANDSGADE
-  // { lat: 56.171588723, lng: 10.190328373 },
-  // { lat: 56.171579336, lng: 10.190250421 },
-  // //DEBUG Bil 2 FINDLANDSGADE
-  // { lat: 56.17158147, lng: 10.19017004 },
-  // { lat: 56.171587902, lng: 10.19009455 },
-  // //DEBUG Bil 3 FINDLANDSGADE
-  // { lat: 56.171587902, lng: 10.19009455 },
-  // { lat: 56.171603129, lng: 10.190022461 },
-  // //DEBUG Bil 4 FINDLANDSGADE
-  // { lat: 56.171676896, lng: 10.189646581 },
-  // { lat: 56.171692074, lng: 10.189546889 },
-];
+let walkingCoordsArray = [];
+let googleCoordsArray = [];
 
-let googleCoordsArray = [
-  // //START SLUT Helsingforsgade
-  // { lat: 56.17138958576229, lng: 10.190254347702766 },
-  // { lat: 56.171798258539255, lng: 10.187848628997688 },
-  // //BIL 1 Helsingforsgade
-  // { lat: 56.171517728447064, lng: 10.189499686305172 },
-  // { lat: 56.17153552375556, lng: 10.189394883149424 },
-  // //BIL 2 Helsingforsgade
-  // { lat: 56.17168590036385, lng: 10.1885093245117 },
-  // { lat: 56.171706691353414, lng: 10.188387069898583 },
-  // //START SLUT Findlandsgade
-  // { lat: 56.17320966989646, lng: 10.191223428358908 },
-  // { lat: 56.17152680907173, lng: 10.19048736251823 },
-  // //BIL 1 Findlandsgade
-  // { lat: 56.17271394314883, lng: 10.191008037104071 },
-  // { lat: 56.172647720395545, lng: 10.190979559854659 },
-  // //BIL 2 Findlandsgade
-  // { lat: 56.17248726614212, lng: 10.190910553809806 },
-  // { lat: 56.17247086696555, lng: 10.19090350110208 },
-];
-
-let magnusArray1 = [
-  //OLD
- {lat:56.171382133, lng: 10.190347083}, 
-  
-];
-let magnusArray2 = [
- {lat:56.17138325, lng: 10.190245717},
-];
+let testArray1 = [];
+let testArray2 = [];
 
 let registeredCarArray = [];
 
@@ -98,7 +38,6 @@ async function fetchParkingSpots() {
         registedCar.oldLng,
         registedCar.newLat,
         registedCar.newLng
-  
       );
     });
 
@@ -213,11 +152,11 @@ function drawWalkingCircles() {
     googleCircles(coords.lat, coords.lng);
   });
 
-  magnusArray1.forEach((coords) => {
-    magnusCircles1(coords.lat, coords.lng);
+  testArray1.forEach((coords) => {
+    testCircles1(coords.lat, coords.lng);
   });
-  magnusArray2.forEach((coords) => {
-    magnusCircles2(coords.lat, coords.lng);
+  testArray2.forEach((coords) => {
+    testCircles2(coords.lat, coords.lng);
   });
 }
 
@@ -231,7 +170,7 @@ function googleCircles(lat, lng) {
   }).addTo(map);
 }
 
-function magnusCircles1(lat, lng) {
+function testCircles1(lat, lng) {
   var circle = L.circle([lat, lng], {
     color: "orange",
     fillColor: "orange",
@@ -240,7 +179,7 @@ function magnusCircles1(lat, lng) {
   }).addTo(map);
 }
 
-function magnusCircles2(lat, lng) {
+function testCircles2(lat, lng) {
   var circle = L.circle([lat, lng], {
     color: "purple",
     fillColor: "purple",
